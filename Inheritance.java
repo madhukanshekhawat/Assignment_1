6fc2832c301380ae4203078b0952dc0fe014bc59
madhukanshekhawat <Madhu.Shekhawat@programmers.io>
2024-08-13 18:22:57 +0530

    inheritance code added

--- Inheritance.java
+++ Inheritance.java
@@ -1,4 +1,9 @@
 public class Inheritance {
+    int veelcount;
+
+    void start(){
+        System.out.println("Vechical is starting");
+    }
 
     public static void main(String[] args) {
 
---
+++ car.java
@@ -0,0 +1,8 @@
+public class car extends Inheritance{
+    public static void main(String[] args) {
+        car c1 = new car();
+        c1.start();
+        c1.veelcount=4;
+        System.out.println("char have "+c1.veelcount +" vehicles");
+    }
+}

09575dc754957b90dd5ba100eab0a64676c1ce80
madhukanshekhawat <Madhu.Shekhawat@programmers.io>
2024-09-03 11:41:03 +0530

    Comment resolved

--- day11/OptionalEx.java
+++ day11/OptionalEx.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 public class OptionalEx {
     public static void main(String[] args) {
         String str="Java 8";
-        Optional<String> optional = Optional.ofNullable(str);
+        Optional<String> optional = Optional.of(str);
         System.out.println(optional.isPresent());
         System.out.println(optional.get());
         System.out.println(optional);
--- day12/GroupingByEx.java
+++ day12/GroupingByEx.java
@@ -9,13 +9,30 @@ public class GroupingByEx {
     public static void main(String[] args) {
         List<Person> personList = new ArrayList<>();
         personList.add(new Person("Madhu","Jaipur",22));
-        personList.add(new Person("Madhu","Jaipur",22));
-        personList.add(new Person("Madhu","Jaipur",23));
-        personList.add(new Person("Madhu","Jaipur",24));
-        personList.add(new Person("Madhu","Jaipur",23));
-        personList.add(new Person("Madhu","Jaipur",23));
+        personList.add(new Person("Goransh","Jaipur",22));
+        personList.add(new Person("Kapil","Delhi",23));
+        personList.add(new Person("Khushi","Jaipur",24));
+        personList.add(new Person("Sonali","Pune",23));
+        personList.add(new Person("Ruchi","Delhi",23));
 
+        System.out.println("Grouping by age : ");
         Map<Integer, List<Person>> groupByAge = personList.stream().collect(Collectors.groupingBy(Person::getAge));
-        System.out.println(personList);
+        System.out.println(groupByAge);
+        System.out.println("Grouping by city : ");
+        Map<String, List<Person>> groupByCity = personList.stream().collect(Collectors.groupingBy(Person::getCity));
+        System.out.println(groupByCity);
+        System.out.println("Group by city then age :");
+        Map<String, Map<Integer, List<Person>>> groupByCityThenAge = personList.stream()
+                .collect(Collectors.groupingBy(Person::getCity, Collectors.groupingBy(Person::getAge)));
+        System.out.println(groupByCityThenAge);
+        System.out.println("Grouping by age");
+        Map<Integer, Long> countByAge = personList.stream()
+                .collect(Collectors.groupingBy(Person::getAge, Collectors.counting()));
+        System.out.println(countByAge);
+        System.out.println("Sum of age : ");
+        Map<String, Integer> sumOfAgesByCity = personList.stream()
+                .collect(Collectors.groupingBy(Person::getCity, Collectors.summingInt(Person::getAge)));
+        System.out.println(sumOfAgesByCity);
+
     }
 }
